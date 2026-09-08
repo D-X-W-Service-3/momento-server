53a4e1fd4905083e64e0898113094bb7c0e8dc5f
sangrae <tiger030205@gmail.com>
2026-09-09 03:41:10 +0900

    feat: 알림 엔티티를 추가한다
    발송 후 읽음 여부만 바뀌어 updated_at 이 없으므로
    BaseCreatedTimeEntity 를 상속한다.

---
+++ src/main/java/com/momento/server/domain/notification/entity/NotificationType.java
@@ -0,0 +1,12 @@
+package com.momento.server.domain.notification.entity;
+
+/** 알림 유형. */
+public enum NotificationType {
+  CAPSULE_INVITE,
+  CAPSULE_RECEIVED,
+  LETTER_MILESTONE,
+  LETTER_DEADLINE,
+  CAPSULE_OPENED,
+  IMAGE_GENERATED,
+  ANNIVERSARY_REMINDER
+}
